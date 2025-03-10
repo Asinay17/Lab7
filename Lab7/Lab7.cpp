acdfa1e74689f44b3cb0cb97d8776a90c374f49e
Ale Sinay <dsinayramos@gmail.com>
2025-03-10 11:17:03 -0600

    se agregaron las cuentas faltantes

--- Lab7/Lab7.cpp
+++ Lab7/Lab7.cpp
@@ -2,8 +2,7 @@
 #include <string>
 
 using namespace std;
-//holaaa
-//holaaa2
+
 class CuentaBancaria {
 private:
     string titular;
@@ -95,24 +94,76 @@ public:
     }
 };
 
+// Creación de 30 cuentas sin usar arreglos ni vectores
+CuentaBancaria c1("Juan Perez", 500);
+CuentaBancaria c2("Maria Lopez", 1200);
+CuentaBancaria c3("Carlos Sanchez", 300);
+CuentaBancaria c4("Luis Gomez", 700);
+CuentaBancaria c5("Ana Martinez", 150);
+CuentaBancaria c6("Pedro Castillo", 1000);
+CuentaBancaria c7("Sofia Ramirez", 400);
+CuentaBancaria c8("Andres Mendoza", 800);
+CuentaBancaria c9("Beatriz Flores", 600);
+CuentaBancaria c10("Roberto Torres", 200);
+CuentaBancaria c11("Elena Vargas", 50);
+CuentaBancaria c12("Fernando Ortega", 900);
+CuentaBancaria c13("Laura Rios", 1100);
+CuentaBancaria c14("Gabriel Soto", 250);
+CuentaBancaria c15("Diana Fernandez", 450);
+CuentaBancaria c16("Oscar Herrera", 350);
+CuentaBancaria c17("Patricia Guzman", 550);
+CuentaBancaria c18("Alberto Jimenez", 650);
+CuentaBancaria c19("Raquel Castro", 750);
+CuentaBancaria c20("Esteban Navarro", 850);
+CuentaBancaria c21("Monica Del Valle", 950);
+CuentaBancaria c22("Ricardo Estrada", 1050);
+CuentaBancaria c23("Lucia Morales", 1150);
+CuentaBancaria c24("Enrique Paredes", 1250);
+CuentaBancaria c25("Valeria Acosta", 1300);
+CuentaBancaria c26("Gustavo Carrillo", 1400);
+CuentaBancaria c27("Carmen Vega", 500);
+CuentaBancaria c28("Jorge Silva", 600);
+CuentaBancaria c29("Mariana Espinoza", 700);
+CuentaBancaria c30("Hugo Alvarado", 800);
 
-//hola
-
-// Crear cuentas
-CuentaBancaria cuenta1("Juan Perez", 500);
-CuentaBancaria cuenta2("Maria Lopez", 1200);
-CuentaBancaria cuenta3("Carlos Sánchez", 300);
-
-// Función para seleccionar la cuenta
+// Función para seleccionar una cuenta
 CuentaBancaria* seleccionarCuenta(int id) {
     switch (id) {
-    case 1: return &cuenta1;
-    case 2: return &cuenta2;
-    case 3: return &cuenta3;
+    case 1: return &c1;
+    case 2: return &c2;
+    case 3: return &c3;
+    case 4: return &c4;
+    case 5: return &c5;
+    case 6: return &c6;
+    case 7: return &c7;
+    case 8: return &c8;
+    case 9: return &c9;
+    case 10: return &c10;
+    case 11: return &c11;
+    case 12: return &c12;
+    case 13: return &c13;
+    case 14: return &c14;
+    case 15: return &c15;
+    case 16: return &c16;
+    case 17: return &c17;
+    case 18: return &c18;
+    case 19: return &c19;
+    case 20: return &c20;
+    case 21: return &c21;
+    case 22: return &c22;
+    case 23: return &c23;
+    case 24: return &c24;
+    case 25: return &c25;
+    case 26: return &c26;
+    case 27: return &c27;
+    case 28: return &c28;
+    case 29: return &c29;
+    case 30: return &c30;
     default: return nullptr;
     }
 }
 
+// Función del menú
 void menu() {
     int opcion;
     do {
@@ -132,7 +183,7 @@ void menu() {
 
         switch (opcion) {
         case 1:
-            cout << "Seleccione cuenta (1-3): ";
+            cout << "Seleccione cuenta (1-30): ";
             cin >> idCuenta;
             cuenta = seleccionarCuenta(idCuenta);
             if (cuenta) {
@@ -140,13 +191,9 @@ void menu() {
                 cin >> monto;
                 cuenta->depositar(monto);
             }
-            else {
-                cout << "Cuenta inválida.\n";
-            }
             break;
-
         case 2:
-            cout << "Seleccione cuenta (1-3): ";
+            cout << "Seleccione cuenta (1-30): ";
             cin >> idCuenta;
             cuenta = seleccionarCuenta(idCuenta);
             if (cuenta) {
@@ -154,16 +201,12 @@ void menu() {
                 cin >> monto;
                 cuenta->retirar(monto);
             }
-            else {
-                cout << "Cuenta inválida.\n";
-            }
             break;
-
         case 3:
-            cout << "Seleccione cuenta origen (1-3): ";
+            cout << "Seleccione cuenta origen (1-30): ";
             cin >> idCuenta;
             cuenta = seleccionarCuenta(idCuenta);
-            cout << "Seleccione cuenta destino (1-3): ";
+            cout << "Seleccione cuenta destino (1-30): ";
             cin >> idDestino;
             destino = seleccionarCuenta(idDestino);
             if (cuenta && destino) {
@@ -171,29 +214,12 @@ void menu() {
                 cin >> monto;
                 cuenta->transferir(*destino, monto);
             }
-            else {
-                cout << "Cuenta inválida.\n";
-            }
             break;
-
         case 4:
-            cout << "Seleccione cuenta (1-3): ";
+            cout << "Seleccione cuenta (1-30): ";
             cin >> idCuenta;
             cuenta = seleccionarCuenta(idCuenta);
-            if (cuenta) {
-                cuenta->mostrarSaldo();
-            }
-            else {
-                cout << "Cuenta inválida.\n";
-            }
-            break;
-
-        case 5:
-            cout << "Saliendo del programa...\n";
-            break;
-
-        default:
-            cout << "Opción no válida. Intente de nuevo.\n";
+            if (cuenta) cuenta->mostrarSaldo();
             break;
         }
     } while (opcion != 5);
@@ -203,5 +229,3 @@ int main() {
     menu();
     return 0;
 }
-
-//cuentas de banco 
